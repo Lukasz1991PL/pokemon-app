37fc9dc37b05844e8ba8fbcc699c448970ff9481
Lukasz Bartkowski <lukasz.bartkowski91@gmail.com>
2022-12-08 21:12:18 +0100

    style button

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -2,25 +2,26 @@ import { useAxios } from '../hook/useAxios';
 import { useParams } from 'react-router-dom';
 
 export const Pokemon = () => {
-  const { id } = useParams();
-  const url = 'https://pokeapi.co/api/v2/pokemon/' + id;
+  const { index } = useParams();
+  const url = 'https://pokeapi.co/api/v2/pokemon/' + index;
   const { data, loading, error } = useAxios(url);
 
-  const { name, height, weight, sprites } = data;
+  const { id, name, height, weight, sprites } = data;
   console.log(url);
   console.log(data);
 
   return (
-    <div className="single-pokemon'">
+    <div className='pokemon'>
       {loading && <div>Is loading</div>}
       {error && <div>{error}</div>}
       <ul>
-        <li>Name:{name}</li>
-        <li>Height:{height}</li>
-        <li>Weight:{weight}</li>
+        <li>#{id}</li>
         <li>
           <img src={sprites?.other.dream_world.front_default} alt='' />
         </li>
+        <li>Name:{name}</li>
+        <li>Height:{height}</li>
+        <li>Weight:{weight}</li>
       </ul>
     </div>
   );
